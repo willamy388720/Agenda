8e4b1238f36305f513d889a198ac6ccf614f7fb8
Willamy Silva <willamygt@gmail.com>
2019-03-29 02:01:12 -0300

    modificação simples

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Notebook
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :"pt-BR"
+    config.assets.initialize_on_precompile = false
   end
 end
